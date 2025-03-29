aa8893a90b89f0196bf33746612f9ee69b51a26e
linerol <sessi.tchecounnou@epitech.eu>
2025-03-29 21:42:53 +0100

    feat: migrate database from MongoDB to PostgreSQL with TypeORM integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,37 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
 import { LoggerModule } from './modules/shared/logger.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [
-        `.env.${process.env.NODE_ENV || 'development'}`,
-        '.env',
-      ],
+      envFilePath: [`.env.${process.env.NODE_ENV || 'development'}`, '.env'],
       load: [appConfig, databaseConfig],
     }),
-    MongooseModule.forRootAsync({
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('uri'),
+        type: configService.get('database.type'),
+        url: configService.get('database.url'),
+        // host: configService.get('database.host'),
+        // port: configService.get('database.port'),
+        // username: configService.get('database.username'),
+        // password: configService.get('database.password'),
+        // database: configService.get('database.database'),
+        synchronize: true,
+        // synchronize: configService.get('database.synchronize'),
+        logging: configService.get('database.logging'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
       inject: [ConfigService],
     }),
-    UsersModule,
-    LoggerModule
+
+    // UsersModule,
+    // LoggerModule
   ],
 })
 export class AppModule {}
