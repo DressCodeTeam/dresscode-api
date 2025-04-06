53095434c532cdc34e0637067c60d4b4b5779801
LuckMeelo <charmeel.vodouhe@epitech.eu>
2025-04-06 15:58:59 +0200

    fix: build error

--- src/common/secret-generator.ts
+++
@@ -1,7 +0,0 @@
-import * as crypto from 'crypto';
-
-export class SecretGenerator {
-  static generateSecret(length: number = 32): string {
-    return crypto.randomBytes(length).toString('hex');
-  }
-}
