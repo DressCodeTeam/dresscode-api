74b9e02478e500ff74506a9d95f958402a3ab900
LuckMeelo <charmeel.vodouhe@epitech.eu>
2025-05-24 20:38:00 +0200

    feat: get user profile

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { UserProfileDto } from './dto/user-profile.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+type GetUserInfosResponse = {
+  get_user_infos: {
+    success: boolean;
+    content: {
+      gender: string;
+      pseudo: string;
+      email: string;
+      birth_date: string;
+    };
+  };
+}[];
 @Injectable()
 export class UsersService {
-  getProfile(userId: string) {
-    return { message: `User profile ${userId}` };
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getProfile(userId: string): Promise<UserProfileDto> {
+    try {
+      const queryResult = (await this.userRepository.query(
+        `SELECT get_user_Infos($1)`,
+        [userId],
+      )) as GetUserInfosResponse;
+
+      const userInfos = queryResult[0]?.get_user_infos;
+
+      if (!userInfos?.success) {
+        throw new InternalServerErrorException(
+          userInfos?.content || 'Failed to fetch user profile',
+        );
+      }
+
+      return userInfos.content;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Error fetching user profile: ${error}`,
+      );
+    }
   }
 }
