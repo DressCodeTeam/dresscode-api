7a65949972a8d5ed12caf136dfcf149bbde9461d
LuckMeelo <charmeel.vodouhe@epitech.eu>
2025-05-28 06:18:03 +0200

    feat: edit outfit response on GET /outfits

--- src/modules/outfits/dto/outfit-response.dto.ts
+++ src/modules/outfits/dto/outfit-response.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { GarmentSummaryDto } from 'src/modules/garments/dto/garment-response.dto';
+import { GarmentResponseDto } from 'src/modules/garments/dto/garment-response.dto';
 
 export class OutfitResponseDto {
   @ApiProperty({
@@ -16,15 +16,9 @@ export class OutfitResponseDto {
 
   @ApiProperty({
     description: 'A list of garments included in the outfit',
-    type: [GarmentSummaryDto],
-    example: [
-      {
-        id: 10,
-        image_url: 'link.com',
-      },
-    ],
+    type: [GarmentResponseDto],
   })
-  garments: GarmentSummaryDto[];
+  garments: GarmentResponseDto[];
 
   @ApiProperty({
     description: 'The date when the outfit was created',
